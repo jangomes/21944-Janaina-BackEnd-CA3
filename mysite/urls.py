815ae7a2521f065cee6ab5a1cd991eaa28709bd6
Janaina Gomes <90685145+jangomes@users.noreply.github.com>
2022-12-02 17:50:26 +0000

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic.base import TemplateView
 
+""" First part: this point the root URLconf at the polls.urls  """
 urlpatterns = [
 
     path('polls/', include('polls.urls')),
