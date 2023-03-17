2727eb76baa69a74ed73a5a771e4c2ae8b76bd00
Janaina Gomes <90685145+jangomes@users.noreply.github.com>
2023-03-17 18:37:43 +0000

    Update tests.py

--- polls/tests.py
+++ polls/tests.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 from .models import Question
 
-
+# That are tests for both the Question model and the views that render it
 class QuestionModelTests(TestCase):
 
     def test_was_published_recently_with_future_question(self):
